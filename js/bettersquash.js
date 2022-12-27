e31698299cc92ebe82655b57d7e8e53b421b9041
Phillip Marlowe <70322156+BetterSquash@users.noreply.github.com>
2022-12-27 19:23:39 +0100

    testing bettersquash.js and share code

---
+++ js/bettersquash.js
@@ -0,0 +1,413 @@
+ * BetterSquash Javascript File
+ */
+ 
+ * Random Popular Article
+ */
+ 
+ var poparticlelinks=new Array()
+
+ poparticlelinks[0]="/2022/20220804.html";
+ poparticlelinks[1]="/2022/20220621.html";
+ poparticlelinks[2]="/2022/20220919.html";
+ poparticlelinks[3]="/2022/20220730.html";
+ poparticlelinks[4]="/2022/20220912.html";
+ poparticlelinks[5]="/2022/20220903.html";
+ poparticlelinks[6]="/2022/20220812.html";
+ poparticlelinks[7]="/2022/20220904.html";
+ poparticlelinks[8]="/2022/20220831.html";
+ poparticlelinks[9]="/2022/20220628.html";
+ poparticlelinks[10]="/2022/20220910.html";
+ poparticlelinks[11]="/2022/20221125.html";
+
+ function RandomPopArticle(){
+ window.location=poparticlelinks[Math.floor(Math.random()*poparticlelinks.length)]
+ }
+ 
+ * Random Article
+ */
+ 
+ var articlelinks=new Array()
+
+ articlelinks[0]="/2022/20220502.html";
+ articlelinks[1]="/2022/20220503.html";
+ articlelinks[2]="/2022/20220504.html";
+ articlelinks[3]="/2022/20220505.html";
+ articlelinks[4]="/2022/20220506.html";
+ articlelinks[5]="/2022/20220507.html";
+ articlelinks[6]="/2022/20220508.html";
+ articlelinks[7]="/2022/20220509.html";
+ articlelinks[8]="/2022/20220510.html";
+ articlelinks[9]="/2022/20220511.html";
+ articlelinks[10]="/2022/20220512.html";
+ articlelinks[11]="/2022/20220513.html";
+ articlelinks[12]="/2022/20220514.html";
+ articlelinks[13]="/2022/20220515.html";
+ articlelinks[14]="/2022/20220516.html";
+ articlelinks[15]="/2022/20220517.html";
+ articlelinks[16]="/2022/20220518.html";
+ articlelinks[17]="/2022/20220519.html";
+ articlelinks[18]="/2022/20220520.html";
+ articlelinks[19]="/2022/20220521.html";
+ articlelinks[20]="/2022/20220522.html";
+ articlelinks[21]="/2022/20220523.html";
+ articlelinks[22]="/2022/20220524.html";
+ articlelinks[23]="/2022/20220525.html";
+ articlelinks[24]="/2022/20220526.html";
+ articlelinks[25]="/2022/20220527.html";
+ articlelinks[26]="/2022/20220528.html";
+ articlelinks[27]="/2022/20220529.html";
+ articlelinks[28]="/2022/20220530.html";
+ articlelinks[29]="/2022/20220531.html";
+ articlelinks[30]="/2022/20220601.html";
+ articlelinks[31]="/2022/20220602.html";
+ articlelinks[32]="/2022/20220603.html";
+ articlelinks[33]="/2022/20220604.html";
+ articlelinks[34]="/2022/20220605.html";
+ articlelinks[35]="/2022/20220606.html";
+ articlelinks[36]="/2022/20220607.html";
+ articlelinks[37]="/2022/20220608.html";
+ articlelinks[38]="/2022/20220609.html";
+ articlelinks[39]="/2022/20220610.html";
+ articlelinks[40]="/2022/20220611.html";
+ articlelinks[41]="/2022/20220612.html";
+ articlelinks[42]="/2022/20220613.html";
+ articlelinks[43]="/2022/20220614.html";
+ articlelinks[44]="/2022/20220615.html";
+ articlelinks[45]="/2022/20220616.html";
+ articlelinks[46]="/2022/20220617.html";
+ articlelinks[47]="/2022/20220618.html";
+ articlelinks[48]="/2022/20220619.html";
+ articlelinks[49]="/2022/20220620.html";
+ articlelinks[50]="/2022/20220621.html";
+ articlelinks[51]="/2022/20220622.html";
+ articlelinks[52]="/2022/20220623.html";
+ articlelinks[53]="/2022/20220624.html";
+ articlelinks[54]="/2022/20220625.html";
+ articlelinks[55]="/2022/20220626.html";
+ articlelinks[56]="/2022/20220627.html";
+ articlelinks[57]="/2022/20220628.html";
+ articlelinks[58]="/2022/20220629.html";
+ articlelinks[59]="/2022/20220630.html";
+ articlelinks[60]="/2022/20220701.html";
+ articlelinks[61]="/2022/20220702.html";
+ articlelinks[62]="/2022/20220703.html";
+ articlelinks[63]="/2022/20220704.html";
+ articlelinks[64]="/2022/20220705.html";
+ articlelinks[65]="/2022/20220706.html";
+ articlelinks[66]="/2022/20220707.html";
+ articlelinks[67]="/2022/20220708.html";
+ articlelinks[68]="/2022/20220709.html";
+ articlelinks[69]="/2022/20220710.html";
+ articlelinks[70]="/2022/20220711.html";
+ articlelinks[71]="/2022/20220712.html";
+ articlelinks[72]="/2022/20220713.html";
+ articlelinks[73]="/2022/20220714.html";
+ articlelinks[74]="/2022/20220715.html";
+ articlelinks[75]="/2022/20220716.html";
+ articlelinks[76]="/2022/20220717.html";
+ articlelinks[77]="/2022/20220718.html";
+ articlelinks[78]="/2022/20220719.html";
+ articlelinks[79]="/2022/20220720.html";
+ articlelinks[80]="/2022/20220721.html";
+ articlelinks[81]="/2022/20220722.html";
+ articlelinks[82]="/2022/20220723.html";
+ articlelinks[83]="/2022/20220724.html";
+ articlelinks[84]="/2022/20220725.html";
+ articlelinks[85]="/2022/20220726.html";
+ articlelinks[86]="/2022/20220727.html";
+ articlelinks[87]="/2022/20220728.html";
+ articlelinks[88]="/2022/20220729.html";
+ articlelinks[89]="/2022/20220730.html";
+ articlelinks[90]="/2022/20220731.html";
+ articlelinks[91]="/2022/20220801.html";
+ articlelinks[92]="/2022/20220802.html";
+ articlelinks[93]="/2022/20220803.html";
+ articlelinks[94]="/2022/20220804.html";
+ articlelinks[95]="/2022/20220805.html";
+ articlelinks[96]="/2022/20220806.html";
+ articlelinks[97]="/2022/20220807.html";
+ articlelinks[98]="/2022/20220808.html";
+ articlelinks[99]="/2022/20220809.html";
+ articlelinks[100]="/2022/20220810.html";
+ articlelinks[101]="/2022/20220811.html";
+ articlelinks[102]="/2022/20220812.html";
+ articlelinks[103]="/2022/20220813.html";
+ articlelinks[104]="/2022/20220814.html";
+ articlelinks[105]="/2022/20220815.html";
+ articlelinks[106]="/2022/20220816.html";
+ articlelinks[107]="/2022/20220817.html";
+ articlelinks[108]="/2022/20220818.html";
+ articlelinks[109]="/2022/20220819.html";
+ articlelinks[110]="/2022/20220820.html";
+ articlelinks[111]="/2022/20220821.html";
+ articlelinks[112]="/2022/20220822.html";
+ articlelinks[113]="/2022/20220823.html";
+ articlelinks[114]="/2022/20220824.html";
+ articlelinks[115]="/2022/20220825.html";
+ articlelinks[116]="/2022/20220826.html";
+ articlelinks[117]="/2022/20220827.html";
+ articlelinks[118]="/2022/20220828.html";
+ articlelinks[119]="/2022/20220829.html";
+ articlelinks[120]="/2022/20220830.html";
+ articlelinks[121]="/2022/20220831.html";
+ articlelinks[122]="/2022/20220901.html";
+ articlelinks[123]="/2022/20220902.html";
+ articlelinks[124]="/2022/20220903.html";
+ articlelinks[125]="/2022/20220904.html";
+ articlelinks[126]="/2022/20220905.html";
+ articlelinks[127]="/2022/20220906.html";
+ articlelinks[128]="/2022/20220907.html";
+ articlelinks[129]="/2022/20220908.html";
+ articlelinks[130]="/2022/20220909.html";
+ articlelinks[131]="/2022/20220910.html";
+ articlelinks[132]="/2022/20220911.html";
+ articlelinks[133]="/2022/20220912.html";
+ articlelinks[134]="/2022/20220913.html";
+ articlelinks[135]="/2022/20220914.html";
+ articlelinks[136]="/2022/20220915.html";
+ articlelinks[137]="/2022/20220916.html";
+ articlelinks[138]="/2022/20220917.html";
+ articlelinks[139]="/2022/20220918.html";
+ articlelinks[140]="/2022/20220919.html";
+ articlelinks[141]="/2022/20220920.html";
+ articlelinks[142]="/2022/20220921.html";
+ articlelinks[143]="/2022/20220922.html";
+ articlelinks[144]="/2022/20220923.html";
+ articlelinks[145]="/2022/20220924.html";
+ articlelinks[146]="/2022/20220925.html";
+ articlelinks[147]="/2022/20220926.html";
+ articlelinks[148]="/2022/20220927.html";
+ articlelinks[149]="/2022/20220928.html";
+ articlelinks[150]="/2022/20220929.html";
+ articlelinks[151]="/2022/20220930.html";
+ articlelinks[152]="/2022/20221001.html";
+ articlelinks[153]="/2022/20221002.html";
+ articlelinks[154]="/2022/20221003.html";
+ articlelinks[155]="/2022/20221004.html";
+ articlelinks[156]="/2022/20221005.html";
+ articlelinks[157]="/2022/20221006.html";
+ articlelinks[158]="/2022/20221007.html";
+ articlelinks[159]="/2022/20221008.html";
+ articlelinks[160]="/2022/20221009.html";
+ articlelinks[161]="/2022/20221010.html";
+ articlelinks[162]="/2022/20221011.html";
+ articlelinks[163]="/2022/20221012.html";
+ articlelinks[164]="/2022/20221013.html";
+ articlelinks[165]="/2022/20221014.html";
+ articlelinks[166]="/2022/20221015.html";
+ articlelinks[167]="/2022/20221016.html";
+ articlelinks[168]="/2022/20221017.html";
+ articlelinks[169]="/2022/20221018.html";
+ articlelinks[170]="/2022/20221019.html";
+ articlelinks[171]="/2022/20221020.html";
+ articlelinks[172]="/2022/20221021.html";
+ articlelinks[173]="/2022/20221022.html";
+ articlelinks[174]="/2022/20221023.html";
+ articlelinks[175]="/2022/20221024.html";
+ articlelinks[176]="/2022/20221025.html";
+ articlelinks[177]="/2022/20221026.html";
+ articlelinks[178]="/2022/20221027.html";
+ articlelinks[179]="/2022/20221028.html";
+ articlelinks[180]="/2022/20221029.html";
+ articlelinks[181]="/2022/20221030.html";
+ articlelinks[182]="/2022/20221031.html";
+ articlelinks[183]="/2022/20221101.html";
+ articlelinks[184]="/2022/20221102.html";
+ articlelinks[185]="/2022/20221103.html";
+ articlelinks[186]="/2022/20221104.html";
+ articlelinks[187]="/2022/20221105.html";
+ articlelinks[188]="/2022/20221106.html";
+ articlelinks[189]="/2022/20221107.html";
+ articlelinks[190]="/2022/20221108.html";
+ articlelinks[191]="/2022/20221109.html";
+ articlelinks[192]="/2022/20221110.html";
+ articlelinks[193]="/2022/20221111.html";
+ articlelinks[194]="/2022/20221112.html";
+ articlelinks[195]="/2022/20221113.html";
+ articlelinks[196]="/2022/20221114.html";
+ articlelinks[197]="/2022/20221115.html";
+ articlelinks[198]="/2022/20221116.html";
+ articlelinks[199]="/2022/20221117.html";
+ articlelinks[200]="/2022/20221118.html";
+ articlelinks[201]="/2022/20221119.html";
+ articlelinks[202]="/2022/20221120.html";
+ articlelinks[203]="/2022/20221121.html";
+ articlelinks[204]="/2022/20221122.html";
+ articlelinks[205]="/2022/20221123.html";
+ articlelinks[206]="/2022/20221124.html";
+ articlelinks[207]="/2022/20221125.html";
+ articlelinks[208]="/2022/20221126.html";
+ articlelinks[209]="/2022/20221127.html";
+ articlelinks[210]="/2022/20221128.html";
+ articlelinks[211]="/2022/20221129.html";
+ articlelinks[212]="/2022/20221130.html";
+ function RandomArticle(){
+ window.location=articlelinks[Math.floor(Math.random()*articlelinks.length)]
+ }
+ 
+/**
+ * A lightweight youtube embed. Still should feel the same to the user, just MUCH faster to initialize and paint.
+ *
+ * Thx to these as the inspiration
+ *   https://storage.googleapis.com/amp-vs-non-amp/youtube-lazy.html
+ *   https://autoplay-youtube-player.glitch.me/
+ *
+ * Once built it, I also found these:
+ *   https://github.com/ampproject/amphtml/blob/master/extensions/amp-youtube (👍👍)
+ *   https://github.com/Daugilas/lazyYT
+ *   https://github.com/vb/lazyframe
+ */
+class LiteYTEmbed extends HTMLElement {
+    connectedCallback() {
+        this.videoId = this.getAttribute('videoid');
+
+        let playBtnEl = this.querySelector('.lty-playbtn');
+        // A label for the button takes priority over a [playlabel] attribute on the custom-element
+        this.playLabel = (playBtnEl && playBtnEl.textContent.trim()) || this.getAttribute('playlabel') || 'Play';
+
+        /**
+         * Lo, the youtube placeholder image!  (aka the thumbnail, poster image, etc)
+         *
+         * See https://github.com/paulirish/lite-youtube-embed/blob/master/youtube-thumbnail-urls.md
+         *
+         * TODO: Do the sddefault->hqdefault fallback
+         *       - When doing this, apply referrerpolicy (https://github.com/ampproject/amphtml/pull/3940)
+         * TODO: Consider using webp if supported, falling back to jpg
+         */
+        if (!this.style.backgroundImage) {
+          this.style.backgroundImage = `url("https://i.ytimg.com/vi/${this.videoId}/hqdefault.jpg")`;
+        }
+
+        // Set up play button, and its visually hidden label
+        if (!playBtnEl) {
+            playBtnEl = document.createElement('button');
+            playBtnEl.type = 'button';
+            playBtnEl.classList.add('lty-playbtn');
+            this.append(playBtnEl);
+        }
+        if (!playBtnEl.textContent) {
+            const playBtnLabelEl = document.createElement('span');
+            playBtnLabelEl.className = 'lyt-visually-hidden';
+            playBtnLabelEl.textContent = this.playLabel;
+            playBtnEl.append(playBtnLabelEl);
+        }
+        playBtnEl.removeAttribute('href');
+
+        // On hover (or tap), warm up the TCP connections we're (likely) about to use.
+        this.addEventListener('pointerover', LiteYTEmbed.warmConnections, {once: true});
+
+        // Once the user clicks, add the real iframe and drop our play button
+        // TODO: In the future we could be like amp-youtube and silently swap in the iframe during idle time
+        //   We'd want to only do this for in-viewport or near-viewport ones: https://github.com/ampproject/amphtml/pull/5003
+        this.addEventListener('click', this.addIframe);
+
+        // Chrome & Edge desktop have no problem with the basic YouTube Embed with ?autoplay=1
+        // However Safari desktop and most/all mobile browsers do not successfully track the user gesture of clicking through the creation/loading of the iframe,
+        // so they don't autoplay automatically. Instead we must load an additional 2 sequential JS files (1KB + 165KB) (un-br) for the YT Player API
+        // TODO: Try loading the the YT API in parallel with our iframe and then attaching/playing it. #82
+        this.needsYTApiForAutoplay = navigator.vendor.includes('Apple') || navigator.userAgent.includes('Mobi');
+    }
+
+    /**
+     * Add a <link rel={preload | preconnect} ...> to the head
+     */
+    static addPrefetch(kind, url, as) {
+        const linkEl = document.createElement('link');
+        linkEl.rel = kind;
+        linkEl.href = url;
+        if (as) {
+            linkEl.as = as;
+        }
+        document.head.append(linkEl);
+    }
+
+    /**
+     * Begin pre-connecting to warm up the iframe load
+     * Since the embed's network requests load within its iframe,
+     *   preload/prefetch'ing them outside the iframe will only cause double-downloads.
+     * So, the best we can do is warm up a few connections to origins that are in the critical path.
+     *
+     * Maybe `<link rel=preload as=document>` would work, but it's unsupported: http://crbug.com/593267
+     * But TBH, I don't think it'll happen soon with Site Isolation and split caches adding serious complexity.
+     */
+    static warmConnections() {
+        if (LiteYTEmbed.preconnected) return;
+
+        // The iframe document and most of its subresources come right off youtube.com
+        LiteYTEmbed.addPrefetch('preconnect', 'https://www.youtube-nocookie.com');
+        // The botguard script is fetched off from google.com
+        LiteYTEmbed.addPrefetch('preconnect', 'https://www.google.com');
+
+        // Not certain if these ad related domains are in the critical path. Could verify with domain-specific throttling.
+        LiteYTEmbed.addPrefetch('preconnect', 'https://googleads.g.doubleclick.net');
+        LiteYTEmbed.addPrefetch('preconnect', 'https://static.doubleclick.net');
+
+        LiteYTEmbed.preconnected = true;
+    }
+
+    fetchYTPlayerApi() {
+        if (window.YT || (window.YT && window.YT.Player)) return;
+
+        this.ytApiPromise = new Promise((res, rej) => {
+            var el = document.createElement('script');
+            el.src = 'https://www.youtube.com/iframe_api';
+            el.async = true;
+            el.onload = _ => {
+                YT.ready(res);
+            };
+            el.onerror = rej;
+            this.append(el);
+        });
+    }
+
+    async addYTPlayerIframe(params) {
+        this.fetchYTPlayerApi();
+        await this.ytApiPromise;
+
+        const videoPlaceholderEl = document.createElement('div')
+        this.append(videoPlaceholderEl);
+
+        const paramsObj = Object.fromEntries(params.entries());
+
+        new YT.Player(videoPlaceholderEl, {
+            width: '100%',
+            videoId: this.videoId,
+            playerVars: paramsObj,
+            events: {
+                'onReady': event => {
+                    event.target.playVideo();
+                }
+            }
+        });
+    }
+
+    async addIframe(){
+        if (this.classList.contains('lyt-activated')) return;
+        this.classList.add('lyt-activated');
+
+        const params = new URLSearchParams(this.getAttribute('params') || []);
+        params.append('autoplay', '1');
+        params.append('playsinline', '1');
+
+        if (this.needsYTApiForAutoplay) {
+            return this.addYTPlayerIframe(params);
+        }
+
+        const iframeEl = document.createElement('iframe');
+        iframeEl.width = 560;
+        iframeEl.height = 315;
+        // No encoding necessary as [title] is safe. https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#:~:text=Safe%20HTML%20Attributes%20include
+        iframeEl.title = this.playLabel;
+        iframeEl.allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture';
+        iframeEl.allowFullscreen = true;
+        // AFAIK, the encoding here isn't necessary for XSS, but we'll do it only because this is a URL
+        // https://stackoverflow.com/q/64959723/89484
+        iframeEl.src = `https://www.youtube-nocookie.com/embed/${encodeURIComponent(this.videoId)}?${params.toString()}`;
+        this.append(iframeEl);
+
+        // Set focus for a11y
+        iframeEl.focus();
+    }
+}
+// Register custom element
+customElements.define('lite-youtube', LiteYTEmbed);
