45ef58c36e2c6b85cb5caae9cb2e9849660f2ae5
Phillip Marlowe <70322156+BetterSquash@users.noreply.github.com>
2024-04-28 19:25:59 +0200

    changes

---
+++ js/randomvideo.js
@@ -0,0 +1,12 @@
+var videolinks=new Array()
+
+videolinks[0]="https://youtu.be/hTXG-0tugUU";
+videolinks[1]="https://youtu.be/4LJjEODRVOY";
+videolinks[2]="https://youtu.be/GJGwUq_jAO4";
+videolinks[3]="https://youtu.be/QzDdDswBYAI";
+videolinks[4]="https://youtu.be/go7GWoSJQxA";
+videolinks[5]="https://youtu.be/-1EeNriiRN0";
+
+function RandomVideo(){
+window.location=videolinks[Math.floor(Math.random()*videolinks.length)]
+}
