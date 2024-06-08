0c60ab84cec6919cbaec348e9cb7bcfbcaf6c648
Phillip Marlowe <70322156+BetterSquash@users.noreply.github.com>
2024-06-08 08:42:37 +0200

    added newest video

--- js/randomvideo.js
+++ js/randomvideo.js
@@ -6,6 +6,7 @@ videolinks[2]="https://youtu.be/GJGwUq_jAO4";
 videolinks[3]="https://youtu.be/QzDdDswBYAI";
 videolinks[4]="https://youtu.be/go7GWoSJQxA";
 videolinks[5]="https://youtu.be/-1EeNriiRN0";
+videolinks[6]="https://youtu.be/j5fuaHMG008";
 
 function RandomVideo(){
 window.location=videolinks[Math.floor(Math.random()*videolinks.length)]
