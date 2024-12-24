0daf379655e3d54dfcc67369ab13329a479689c7
Phillip Marlowe <70322156+BetterSquash@users.noreply.github.com>
2024-12-24 16:13:31 +0000

    updated the random video list

--- js/randomvideo.js
+++ js/randomvideo.js
@@ -7,6 +7,18 @@ videolinks[3]="https://youtu.be/QzDdDswBYAI";
 videolinks[4]="https://youtu.be/go7GWoSJQxA";
 videolinks[5]="https://youtu.be/-1EeNriiRN0";
 videolinks[6]="https://youtu.be/j5fuaHMG008";
+videolinks[7]="https://youtu.be/sseiDAU55io";
+videolinks[8]="https://youtu.be/r-APDRR2PsU";
+videolinks[9]="https://youtu.be/25curvTaz7k";
+videolinks[10]="https://youtu.be/U8OQ_p8c14o";
+videolinks[11]="https://youtu.be/gTLmqy9UjWM";
+videolinks[12]="https://youtu.be/QSSBg8ZjhJ0";
+videolinks[13]="https://youtu.be/irZvc8_wCNc";
+videolinks[14]="https://youtu.be/5yPegJX79YM";
+videolinks[15]="https://youtu.be/A8f_ItrZ9Z8";
+videolinks[16]="https://youtu.be/mbVCEjXcyN4";
+videolinks[17]="https://youtu.be/Tsl3liXoc6M";
+videolinks[18]="https://youtu.be/j5fuaHMG008";
 
 function RandomVideo(){
 window.location=videolinks[Math.floor(Math.random()*videolinks.length)]
