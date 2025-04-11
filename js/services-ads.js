9bc1522f517884b4db8845e606811fdbdf5f8bf3
Phillip Marlowe <70322156+BetterSquash@users.noreply.github.com>
2025-04-11 13:48:02 +0200

    Update services-ads.js

--- js/services-ads.js
+++ js/services-ads.js
@@ -31,7 +31,7 @@
     const container = document.getElementById('service-cta');
     if (container) {
       container.innerHTML = `
-        <div style="border: 1px solid #000; margin-top: 1rem; margin-bottom: 2rem; padding-top: 1.5rem; font-family: sans-serif;" background: #ddd>
+        <div style="border: 1px solid #ccc; margin-top: 1rem; margin-bottom: 1rem; padding: 1rem">
           <H2>${promo.title}</H2><br>
           <span>${promo.text}</span><br>
           <a href="${promo.link}">Learn more</a>
